827ffd5d1370bc69ba9979210cb83919e0b67ade
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-02 11:29:42 -0800

    Header auth state and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Header.css';
 
-const Header = ({ authSubmit, inputChange }) => {
+const Header = ({ isSignedIn, dancerName, authSubmit, inputChange, logoutClick }) => {
   return (
     <header>
       <h1>Morris Managed</h1>
-      <form onSubmit={authSubmit}>
-        <input type="text" name="email" placeholder="email" onChange={inputChange} />
-        <input type="password" name="password" placeholder="password" onChange={inputChange} />
-        <button>Sign in</button>
-      </form>
+      {isSignedIn ? 
+        <div>Welcome, {dancerName} <button onClick={logoutClick}>Sign out</button></div> : 
+        <form onSubmit={authSubmit}>
+          <input type="text" name="email" placeholder="email" onChange={inputChange} />
+          <input type="password" name="password" placeholder="password" onChange={inputChange} />
+          <button>Sign in</button>
+        </form>
+      }
     </header>
   );
 };
 
 Header.propTypes = {
+  isSignedIn: PropTypes.bool.isRequired,
+  dancerName: PropTypes.string,
   authSubmit: PropTypes.func.isRequired,
-  inputChange: PropTypes.func.isRequired
+  inputChange: PropTypes.func.isRequired,
+  logoutClick: PropTypes.func.isRequired
 };
 
 export default Header;
