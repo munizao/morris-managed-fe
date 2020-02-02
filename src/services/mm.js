339294271449056787093daedfcc16e097e4adf9
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-02 06:32:18 -0800

    got auth services working

--- src/services/mm.js
+++ src/services/mm.js
@@ -1,8 +1,18 @@
 const API_URL = 'https://morris-managed.herokuapp.com/api/v1';
+// const API_URL = 'http://localhost:7890/api/v1';
 
-export const signIn = (email, password) => {
+
+export const login = (email, password) => {
   const body = { email, password };
-  fetch(`${API_URL}/login`, { method: 'POST', credentials: 'include', body })
+  return fetch(`${API_URL}/auth/login`, { method: 'POST', 
+    body: JSON.stringify(body), 
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include' })
+    .then(res => res.json());
+};
+
+export const signedIn = () => {
+  return fetch(`${API_URL}/auth/signed-in`, { method: 'GET', credentials: 'include' })
     .then(res => res.json());
 };
 
