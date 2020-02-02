0bfc30fabc04f503cf51b795abdb849bf6f53b96
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-02 06:31:12 -0800

    hook auth up to form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ authSubmit, inputChange }) => {
   return (
     <header>
       <h1>Morris Managed</h1>
-      <input placeholder="email" value="email" />
-      <input placeholder="password" value="password" />
+      <form onSubmit={authSubmit}>
+        <input type="text" name="email" placeholder="email" onChange={inputChange} />
+        <input type="password" name="password" placeholder="password" onChange={inputChange} />
+        <button>Sign in</button>
+      </form>
     </header>
   );
+};
+
+Header.propTypes = {
+  authSubmit: PropTypes.func.isRequired,
+  inputChange: PropTypes.func.isRequired
 };
 
 export default Header;
