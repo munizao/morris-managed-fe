f58b7dc9d7e20b58564fb74bcb0f7ca1ed520f61
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-03 09:03:55 -0800

    mocks

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+jest.mock('../services/mm.js', () => ({
+  signedIn: jest.fn(() => Promise.resolve()),
+  allDances: jest.fn(() => Promise.resolve())
+}));
+
 describe('App component', () => {
   it('renders App', () => {
     const wrapper = shallow(<App />);
